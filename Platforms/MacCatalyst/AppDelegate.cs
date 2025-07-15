ccb0b54828efa4d7854139e0f1b387e2ff32e7cc
techit-oss <phoebe.lousie.farnsworth@outlook.com>
2025-07-15 03:12:31 +0100

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPasswordManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
